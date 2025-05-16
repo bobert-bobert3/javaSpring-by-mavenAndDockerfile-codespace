e79c3ebe7e8276adc9859a7d785b7e812b7732c7
bobert-bobert3 <bobert.bobert3@hotmail.com>
2025-05-16 13:51:10 -0400

    Add files via upload
    Uploaded files from Spring Boot > Spring Web.

---
+++ src/test/java/rhafen/docker/example/springWebContainer/SpringWebContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package rhafen.docker.example.springWebContainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebContainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
